34d69e91ac7900280d2b96b531eba8710ae8c1e8
Matt Luke <mlukedev@icloud.com>
2020-02-22 16:36:36 -0500

    Added cells to Collections page

---
+++ ACNH App/CollectionCell.swift
@@ -0,0 +1,31 @@
+//
+//  CollectionCell.swift
+//  ACNH App
+//
+//  Created by Matt Luke on 2/22/20.
+//  Copyright © 2020 MlukeApps. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell: UITableViewCell {
+
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        cellView.layer.cornerRadius = 15
+        iconView.layer.cornerRadius = 15
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ACNH App/CollectionsVC.swift
@@ -0,0 +1,48 @@
+//
+//  CollectionsVC.swift
+//  ACNH App
+//
+//  Created by Matt Luke on 2/22/20.
+//  Copyright © 2020 MlukeApps. All rights reserved.
+//
+
+import UIKit
+
+class CollectionsVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "collectionCell", for: indexPath) as! CollectionCell
+
+        return cell
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
